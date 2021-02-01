b4a285fb826d363d6773db9c683bcf98113a9acb
Luiz Marques <passport@stgsys.com>
2021-02-01 14:33:08 -0300

    module 4 - trying simpler import...

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -1,11 +1,12 @@
-from ssg import (hooks, parsers)
+from ssg import hooks
+from ssg import parsers
 
 files = []
 
 
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    valid = lambda p: p is isinstance(parsers.ResourceParser)
+    valid = lambda p: not isinstance(p,parsers.ResourceParser)
     for path in source.rglob("*"):
         for parser in list(filter(valid, site_parsers)):
             if parser.valid_file_ext():
